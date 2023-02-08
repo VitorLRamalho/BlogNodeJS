780620e1a00f12b5d2f9f3634fb55f1851a9c231
Vitor Ramalho <vitorlramalho@gmail.com>
2023-02-08 15:13:21 -0300

    commit add bootstrap

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 const admin = require("./routes/admin")
+const path = require("path")
 //const mongoose = require('mongoose');
 
 //configurações
@@ -13,12 +14,22 @@ const admin = require("./routes/admin")
     app.engine('handlebars',handlebars.engine({defaulyLayout:'main'}));
     app.set('view engine', 'handlebars');
     //mongoose
-
+        //
+    //public
+    app.use(express.static(path.join(__dirname, "public")));
 //rotas
-    app.use('/admin', admin)
+   /* app.use('/', (req, res) => {
+        res.send('Rota principal')
+    });
+
+    app.get('/posts', (req, res) => {
+        res.send("Lista Posts")
+    });
+    */
+    app.use('/admin', admin);
 
 //outros
 const PORTA = 8081;
 app.listen(PORTA, () => {
-    console.log("Servidor Rodando na porta: (http://8081)" )
+    console.log("Servidor Rodando na porta: (http://localhost:8081)" )
 });
--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,20 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.send("Página principal do painel ADM")
+    res.render("admin/index")
 });
 
 router.get('/posts', (req, res) => {
     res.send("Página de posts")
 }); 
 
-router.get("/Categorias", (req, res) => {
+router.get('/Categorias', (req, res) => {
     res.send("Página de categorias")
 });
 
+router.get('/teste', (req, res) => {
+    res.send("isso é um teste!!!")
+});
 
 //export do "router"(final do arquivo)
 module.exports = router
